8b5393b14092100d443904a1e3a72681cca1b433
shturnev <sht_job@ukr.net>
2017-05-22 23:57:56 +0300

    +

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@
  * Created by sht-home on 22.05.2017.
  */
 
-var gulp         = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var cssmin = require('gulp-cssmin');
+var gulp            = require('gulp');
+var autoprefixer    = require('gulp-autoprefixer');
+var cssmin          = require('gulp-cssmin');
+var browserSync     = require('browser-sync').create();
 
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        //proxy: "mnschool"
+        proxy: "localhost:63342/maket9"
+        ,notify: false
+    });
+});
 
 
 
